fee59ce675fbba064382bd2eb63cbb3feed0020c
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-16 23:08:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 require 'pry'
 
  def reverse_each_word(sentence)
- original_array = sentence.split(" ") #set the split sentence to a new value
+ array = sentence.split(" ") #set the split sentence to an array
    new_array = [] #created a new_array
-   original_array.collect do |words|
+   array.collect do |words|
      new_array << words.reverse
    end
    new_array.join(" ")
